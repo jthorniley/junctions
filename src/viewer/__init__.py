a3905af4240516edde45fb8c54e6ab678cf3e25c
James Thorniley <james.thorniley@gmail.com>
2023-08-06 09:54:41 +0100

    create an arcade window

--- src/viewer/__init__.py
+++ src/viewer/__init__.py
@@ -1,2 +1,17 @@
+import arcade
+import arcade.color
+
+
+class JunctionsWindow(arcade.Window):
+    def __init__(self):
+        screens = arcade.get_screens()
+        super().__init__(800, 800, "Junctions", screen=screens[0])
+        self.background_color = arcade.color.BUD_GREEN
+
+    def on_draw(self):
+        self.clear()
+
+
 def run():
-    print("hello world")
+    window = JunctionsWindow()
+    window.run()
