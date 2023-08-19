3d01a877414c845629df6d2cc64a69ca64d0ff45
James Thorniley <james.thorniley@gmail.com>
2023-08-19 07:46:20 +0100

    add arc model

--- src/junctions/types.py
+++ src/junctions/types.py
@@ -11,7 +11,7 @@ class Road:
     `bearing` is the angle north that the A lane travels:
 
     * 0 -> A runs south-north (towards +ve y)
-    * PI/2 -> A runs east-west (towards +ve x)
+    * PI/2 -> A runs west-east (towards +ve x)
     * PI -> A runs north-south (towards -ve y)
     * etc
 
@@ -21,4 +21,33 @@ class Road:
     origin: tuple[float, float]
     bearing: float
     road_length: float
+    lane_separation: float
+
+
+@dataclass(frozen=True)
+class Arc:
+    """Two lane road following an arc curve.
+
+    Two lanes (A and B) follow a circular arc.
+
+    A starts at `origin` with given `bearing`.
+
+    B is parallel to A but runs in the opposite direction. The
+    `lane_separation` defines the distance between B and A.
+
+    `arc_radius` defines the curvature radius for the inner (A) lane.
+
+    The `arc_length` is how far A curves around which can be from
+    `0` to `2*PI` (negative values not allowed).
+
+    The A lane is always the "inner" lane of the curve, and "B" the
+    outer.
+
+    ![](images/arc.png)
+    """
+
+    origin: tuple[float, float]
+    bearing: float
+    arc_length: float
+    arc_radius: float
     lane_separation: float
