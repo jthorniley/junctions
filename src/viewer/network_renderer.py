49f9af8f027c3cf9c70f45910bec8cc10db06f74
James Thorniley <james.thorniley@gmail.com>
2023-08-20 21:00:08 +0100

    confirm behaviour of network renderer

--- src/viewer/network_renderer.py
+++ src/viewer/network_renderer.py
@@ -6,7 +6,7 @@ from junctions.types import Arc, Junction, Lane, Road
 from pyglet.math import Vec2
 
 
-def node_markers(
+def _node_markers(
     lane: Lane, batch: pyglet.shapes.Batch
 ) -> Sequence[pyglet.shapes.ShapeBase]:
     a = pyglet.shapes.Circle(
@@ -26,82 +26,98 @@ def node_markers(
     return (a, b)
 
 
+def _road_shapes(
+    road: Road, batch: pyglet.graphics.Batch
+) -> Sequence[pyglet.shapes.ShapeBase]:
+    lanes = road.lanes
+    lane_a = pyglet.shapes.Line(
+        lanes[0].start.x,
+        lanes[0].start.y,
+        lanes[0].end.x,
+        lanes[0].end.y,
+        color=(103, 240, 90, 255),
+        batch=batch,
+    )
+    lane_b = pyglet.shapes.Line(
+        lanes[1].start.x,
+        lanes[1].start.y,
+        lanes[1].end.x,
+        lanes[1].end.y,
+        color=(103, 240, 90, 255),
+        batch=batch,
+    )
+
+    return (
+        lane_a,
+        lane_b,
+        *_node_markers(lanes[0], batch),
+        *_node_markers(lanes[1], batch),
+    )
+
+
+def _arc_shapes(
+    arc: Arc, batch: pyglet.graphics.Batch
+) -> Sequence[pyglet.shapes.ShapeBase]:
+    lanes = arc.lanes
+    a0 = lanes[0].start
+    b1 = lanes[1].end
+    focus = arc.focus
+
+    def make_line(start_point, r):
+        point = start_point
+        lines = []
+        n_points = int(max(10, (r**2) / 10))
+        for i in range(n_points):
+            angle = -arc.bearing - arc.arc_length * (i / (n_points - 1))
+            next_point = focus + Vec2(-r, 0).rotate(angle)
+            lines.append(
+                pyglet.shapes.Line(
+                    point.x,
+                    point.y,
+                    next_point.x,
+                    next_point.y,
+                    color=(103, 240, 90, 255),
+                    batch=batch,
+                )
+            )
+            point = next_point
+
+        return lines
+
+    lane_a = make_line(a0, arc.arc_radius)
+    b_radius = arc.arc_radius + arc.lane_separation
+    lane_b = make_line(b1, b_radius)
+    return (
+        *lane_a,
+        *lane_b,
+        *_node_markers(lanes[0], batch),
+        *_node_markers(lanes[1], batch),
+    )
+
+
 class NetworkRenderer:
     def __init__(self, network: Network):
-        self._network = network
+        self._network: Network = network
 
-        self.junctions: dict[Junction, Sequence[pyglet.shapes.ShapeBase]] = {}
-        self.batch: pyglet.graphics.Batch = pyglet.graphics.Batch()
+        self._junctions: dict[str, Sequence[pyglet.shapes.ShapeBase]] = {}
+        self._batch: pyglet.graphics.Batch = pyglet.graphics.Batch()
 
-        for junction in self._network.all_junctions():
-            self._add_junction(junction)
-
-    def _add_junction(self, junction: Junction):
-        match junction:
-            case Road():
-                lanes = junction.lanes
-                lane_a = pyglet.shapes.Line(
-                    lanes[0].start.x,
-                    lanes[0].start.y,
-                    lanes[0].end.x,
-                    lanes[0].end.y,
-                    color=(103, 240, 90, 255),
-                    batch=self.batch,
-                )
-                lane_b = pyglet.shapes.Line(
-                    lanes[1].start.x,
-                    lanes[1].start.y,
-                    lanes[1].end.x,
-                    lanes[1].end.y,
-                    color=(103, 240, 90, 255),
-                    batch=self.batch,
-                )
-
-                self.junctions[junction] = (
-                    lane_a,
-                    lane_b,
-                    *node_markers(lanes[0], self.batch),
-                    *node_markers(lanes[1], self.batch),
-                )
-
-            case Arc():
-                lanes = junction.lanes
-                a0 = lanes[0].start
-                b1 = lanes[1].end
-                focus = junction.focus
-
-                def make_line(start_point, r):
-                    point = start_point
-                    lines = []
-                    n_points = int(max(10, (r**2) / 10))
-                    for i in range(n_points):
-                        angle = -junction.bearing - junction.arc_length * (
-                            i / (n_points - 1)
-                        )
-                        next_point = focus + Vec2(-r, 0).rotate(angle)
-                        lines.append(
-                            pyglet.shapes.Line(
-                                point.x,
-                                point.y,
-                                next_point.x,
-                                next_point.y,
-                                color=(103, 240, 90, 255),
-                                batch=self.batch,
-                            )
-                        )
-                        point = next_point
-
-                    return lines
-
-                lane_a = make_line(a0, junction.arc_radius)
-                b_radius = junction.arc_radius + junction.lane_separation
-                lane_b = make_line(b1, b_radius)
-                self.junctions[junction] = (
-                    *lane_a,
-                    *lane_b,
-                    *node_markers(lanes[0], self.batch),
-                    *node_markers(lanes[1], self.batch),
-                )
+    def _refresh_batch(self):
+        junction_lookup = self._network.junction_lookup
+        if junction_lookup.keys() != self._junctions.keys():
+            self._junctions = {}
+            self._batch = pyglet.graphics.Batch()
+            for label, junction in junction_lookup.items():
+                self._add_junction(label, junction)
 
     def draw(self):
-        self.batch.draw()
+        self._refresh_batch()
+        self._batch.draw()
+
+    def _add_junction(self, label: str, junction: Junction):
+        match junction:
+            case Road():
+                self._junctions[label] = _road_shapes(junction, self._batch)
+
+            case Arc():
+                self._junctions[label] = _arc_shapes(junction, self._batch)
--- tests/viewer/test_network_renderer.py
+++ tests/viewer/test_network_renderer.py
@@ -1,4 +1,5 @@
 from io import BytesIO
+from unittest import mock
 
 import pyglet
 import pytest
@@ -71,4 +72,69 @@ def test_render_road_arc(reference_render: ReferenceRender, _fuzz):
     NetworkRenderer(network).draw()
 
     # THEN the network is as expected
+    reference_render.assert_screenshots_match()
+
+
+def test_render_change_in_network(reference_render: ReferenceRender):
+    # GIVEN a network with a road junction
+    network = Network()
+
+    # AND a network renderer
+    network_renderer = NetworkRenderer(network)
+
+    # WHEN I add the junction after the renderer is created
+    network.add_junction(RoadFactory.build())
+
+    # AND render it
+    network_renderer.draw()
+
+    # THEN the network is as expected
+    reference_render.assert_screenshots_match()
+
+
+def test_render_change_in_network_only_if_necessary(reference_render: ReferenceRender):
+    """This tests that the network renderer only refreshes its shape batch
+    when necessary (i.e. if the underlying network has changed)
+    """
+    # GIVEN a network with a road junction
+    network = Network()
+
+    # AND a network renderer
+    network_renderer = NetworkRenderer(network)
+
+    with mock.patch.object(
+        network_renderer, "_add_junction", side_effect=network_renderer._add_junction
+    ) as add_junction:
+        # WHEN I add the junction after the renderer is created
+        network.add_junction(RoadFactory.build())
+
+        # AND render it
+        network_renderer.draw()
+
+        # THEN add junction is called in the renderer
+        add_junction.assert_called_once_with("road1", network.junction_lookup["road1"])
+        add_junction.reset_mock()
+
+        # AND WHEN I render it again
+        network_renderer.draw()
+
+        # THEN no new shapes need to be created
+        add_junction.assert_not_called()
+        add_junction.reset_mock()
+
+        # AND WHEN I add another junction and render
+        network.add_junction(RoadFactory.build())
+
+        # AND render it
+        network_renderer.draw()
+
+        # THEN the junctions are added again
+        add_junction.assert_has_calls(
+            [
+                mock.call("road1", network.junction_lookup["road1"]),
+                mock.call("road2", network.junction_lookup["road2"]),
+            ]
+        )
+
+    # for good measure, check the screenshot is unchanged
     reference_render.assert_screenshots_match()
