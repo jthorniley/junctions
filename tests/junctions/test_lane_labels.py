89eb93b7c186175ccb5d1368335a267109b65161
James Thorniley <james.thorniley@gmail.com>
2023-08-27 10:33:08 +0100

    Tests for lane labels

---
+++ tests/junctions/test_lane_labels.py
@@ -0,0 +1,21 @@
+from junctions.types import Arc
+
+from tests.junctions.factories import ArcFactory, RoadFactory, TeeFactory
+
+# test that the LANE_LABELS lists match up to the keys created
+# by the lanes property - it always should
+
+
+def test_road_lane_labels():
+    road = RoadFactory.build()
+    assert tuple(road.lanes.keys()) == road.LANE_LABELS
+
+
+def test_arc_lane_labels():
+    arc = ArcFactory.build()
+    assert tuple(arc.lanes.keys()) == Arc.LANE_LABELS
+
+
+def test_tee_lane_labels():
+    tee = TeeFactory.build()
+    assert tuple(tee.lanes.keys()) == tee.LANE_LABELS
