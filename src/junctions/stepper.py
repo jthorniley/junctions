6605754d849469ec07dbc703eb046f36cccc23bd
James Thorniley <james.thorniley@gmail.com>
2023-09-16 10:07:11 +0100

    rewrite stepper api/tests

--- src/junctions/stepper.py
+++ src/junctions/stepper.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING, Final
 
 from junctions.network import LaneRef
 from junctions.priority_wait import priority_wait
+from junctions.state.vehicle_positions import VehiclePositions
 from junctions.state.vehicles import (
     _Vehicle,
     _VehiclesState,
@@ -22,99 +23,105 @@ VEHICLE_SEPARATION_LIMIT: Final = 5
 class Stepper:
     """Utility for stepping the simulation in time"""
 
-    def __init__(self, network: Network):
-        self._network = network
-        self._wait_flags: WaitFlags | None = None
-        self._vehicle_planned_next_lane: dict[str, LaneRef] = {}
+    def __init__(self, network: Network, vehicle_positions: VehiclePositions) -> None:
+        ...
 
-    def _calculate_vehicle_update(
-        self, dt: float, vehicle_label: str, vehicle: _Vehicle
-    ) -> _Vehicle | None:
-        # the algorithm is defined in doc/03-vehicles.md
-        speed = self._network.speed_limit(vehicle.lane_ref)
-        movement = speed * dt
+    def step(self, dt: float) -> None:
+        ...
 
-        next_position = vehicle.position + movement
+    # def __init__(self, network: Network):
+    #     self._network = network
+    #     self._wait_flags: WaitFlags | None = None
+    #     self._vehicle_planned_next_lane: dict[str, LaneRef] = {}
 
-        lane = self._network.lane(vehicle.lane_ref)
+    # def _calculate_vehicle_update(
+    #     self, dt: float, vehicle_label: str, vehicle: _Vehicle
+    # ) -> _Vehicle | None:
+    #     # the algorithm is defined in doc/03-vehicles.md
+    #     speed = self._network.speed_limit(vehicle.lane_ref)
+    #     movement = speed * dt
 
-        if (excess := next_position - lane.length) > 0:
-            next_lane_ref = None
+    #     next_position = vehicle.position + movement
 
-            if vehicle_label in self._vehicle_planned_next_lane:
-                next_lane_ref = self._vehicle_planned_next_lane[vehicle_label]
-            else:
-                next_lane_choices = self._network.connected_lanes(vehicle.lane_ref)
+    #     lane = self._network.lane(vehicle.lane_ref)
 
-                if next_lane_choices:
-                    next_lane_ref = random.choice(next_lane_choices)
-                    self._vehicle_planned_next_lane[vehicle_label] = next_lane_ref
+    #     if (excess := next_position - lane.length) > 0:
+    #         next_lane_ref = None
 
-            if next_lane_ref:
-                if self._wait_flags and self._wait_flags[next_lane_ref]:
-                    # Next lane is wait, set vehicle to end of current lane
-                    return _Vehicle(lane_ref=vehicle.lane_ref, position=lane.length)
-                else:
-                    t_excess = excess / speed
+    #         if vehicle_label in self._vehicle_planned_next_lane:
+    #             next_lane_ref = self._vehicle_planned_next_lane[vehicle_label]
+    #         else:
+    #             next_lane_choices = self._network.connected_lanes(vehicle.lane_ref)
 
-                    next_lane_speed_limit = self._network.speed_limit(next_lane_ref)
+    #             if next_lane_choices:
+    #                 next_lane_ref = random.choice(next_lane_choices)
+    #                 self._vehicle_planned_next_lane[vehicle_label] = next_lane_ref
 
-                    try:
-                        self._vehicle_planned_next_lane.pop(vehicle_label)
-                    except KeyError:
-                        ...
+    #         if next_lane_ref:
+    #             if self._wait_flags and self._wait_flags[next_lane_ref]:
+    #                 # Next lane is wait, set vehicle to end of current lane
+    #                 return _Vehicle(lane_ref=vehicle.lane_ref, position=lane.length)
+    #             else:
+    #                 t_excess = excess / speed
 
-                    return _Vehicle(
-                        lane_ref=next_lane_ref,
-                        position=t_excess * next_lane_speed_limit,
-                    )
+    #                 next_lane_speed_limit = self._network.speed_limit(next_lane_ref)
 
-            else:
-                return None
-        else:
-            return _Vehicle(
-                lane_ref=vehicle.lane_ref,
-                position=next_position,
-            )
+    #                 try:
+    #                     self._vehicle_planned_next_lane.pop(vehicle_label)
+    #                 except KeyError:
+    #                     ...
 
-    def step(self, dt: float, vehicles_state: _VehiclesState) -> _VehiclesState:
-        """Perform a step with time interval dt"""
+    #                 return _Vehicle(
+    #                     lane_ref=next_lane_ref,
+    #                     position=t_excess * next_lane_speed_limit,
+    #                 )
 
-        self._wait_flags = priority_wait(self._network, vehicles_state)
+    #         else:
+    #             return None
+    #     else:
+    #         return _Vehicle(
+    #             lane_ref=vehicle.lane_ref,
+    #             position=next_position,
+    #         )
 
-        next_vehicles_state = _VehiclesState()
+    # def step(self, dt: float, vehicles_state: _VehiclesState) -> _VehiclesState:
+    #     """Perform a step with time interval dt"""
 
-        lane_vehicle_map: defaultdict[LaneRef, list[tuple[str, float]]] = defaultdict(
-            list
-        )
-        for vehicle_label, vehicle in vehicles_state.items():
-            lane_vehicle_map[vehicle.lane_ref].append((vehicle_label, vehicle.position))
+    #     self._wait_flags = priority_wait(self._network, vehicles_state)
 
-        for _, vehicles in lane_vehicle_map.items():
-            vehicles.sort(key=itemgetter(1))
+    #     next_vehicles_state = _VehiclesState()
 
-        for lane_ref, lane_vehicles in lane_vehicle_map.items():
-            for vehicle_index, (vehicle_label, vehicle_position) in enumerate(
-                lane_vehicles
-            ):
-                if vehicle_index < len(lane_vehicles) - 1:
-                    next_vehicle_position = lane_vehicles[vehicle_index + 1][1]
-                    if next_vehicle_position < (
-                        vehicle_position + VEHICLE_SEPARATION_LIMIT
-                    ):
-                        next_vehicles_state.add_vehicle(
-                            _Vehicle(lane_ref, vehicle_position), vehicle_label
-                        )
-                        continue
+    #     lane_vehicle_map: defaultdict[LaneRef, list[tuple[str, float]]] = defaultdict(
+    #         list
+    #     )
+    #     for vehicle_label, vehicle in vehicles_state.items():
+    #         lane_vehicle_map[vehicle.lane_ref].append((vehicle_label, vehicle.position))
 
-                next_vehicle = self._calculate_vehicle_update(
-                    dt, vehicle_label, _Vehicle(lane_ref, vehicle_position)
-                )
-                if next_vehicle is not None:
-                    next_vehicles_state.add_vehicle(next_vehicle, vehicle_label)
+    #     for _, vehicles in lane_vehicle_map.items():
+    #         vehicles.sort(key=itemgetter(1))
 
-        return next_vehicles_state
+    #     for lane_ref, lane_vehicles in lane_vehicle_map.items():
+    #         for vehicle_index, (vehicle_label, vehicle_position) in enumerate(
+    #             lane_vehicles
+    #         ):
+    #             if vehicle_index < len(lane_vehicles) - 1:
+    #                 next_vehicle_position = lane_vehicles[vehicle_index + 1][1]
+    #                 if next_vehicle_position < (
+    #                     vehicle_position + VEHICLE_SEPARATION_LIMIT
+    #                 ):
+    #                     next_vehicles_state.add_vehicle(
+    #                         _Vehicle(lane_ref, vehicle_position), vehicle_label
+    #                     )
+    #                     continue
 
-    @property
-    def wait_flags(self) -> WaitFlags | None:
-        return self._wait_flags
+    #             next_vehicle = self._calculate_vehicle_update(
+    #                 dt, vehicle_label, _Vehicle(lane_ref, vehicle_position)
+    #             )
+    #             if next_vehicle is not None:
+    #                 next_vehicles_state.add_vehicle(next_vehicle, vehicle_label)
+
+    #     return next_vehicles_state
+
+    # @property
+    # def wait_flags(self) -> WaitFlags | None:
+    #     return self._wait_flags
--- tests/junctions/test_stepper.py
+++ tests/junctions/test_stepper.py
@@ -1,6 +1,6 @@
 import pytest
 from junctions.network import LaneRef, Network
-from junctions.state.vehicles import _Vehicle, _VehiclesState
+from junctions.state.vehicle_positions import VehiclePositions
 from junctions.stepper import Stepper
 from junctions.types import Road
 
@@ -13,20 +13,20 @@ def test_simple_step():
     network.add_junction(RoadFactory.build(), label="theroad")
 
     # ... and a vehicle on that road
-    vehicles = _VehiclesState()
-    vehicles.add_vehicle(_Vehicle(LaneRef("theroad", "a"), 0.0), "v1")
+    vehicles = VehiclePositions()
+    v1 = vehicles.create_vehicle(LaneRef("theroad", "a"), 0.0)
 
     # ... and a stepper constructed with the network
-    stepper = Stepper(network)
+    stepper = Stepper(network, vehicles)
 
     # WHEN I step
-    next_vehicle_state = stepper.step(0.1, vehicles)
+    stepper.step(0.1)
 
     # THEN the vehicle has moved
-    vehicle = next_vehicle_state.vehicle("v1")
-    assert vehicle.lane_ref.junction == "theroad"
-    assert vehicle.lane_ref.lane == "a"
-    assert vehicle.position == pytest.approx(0.65)
+    vehicle = vehicles[v1]
+    assert vehicle["lane_ref"].junction == "theroad"
+    assert vehicle["lane_ref"].lane == "a"
+    assert vehicle["position"] == pytest.approx(0.65)
 
 
 def test_step_to_next_lane():
@@ -43,94 +43,91 @@ def test_step_to_next_lane():
     network.connect_lanes(LaneRef("second_road", "b"), LaneRef("first_road", "b"))
 
     # ... first vehicle is traversing the first road
-    vehicles = _VehiclesState()
-    vehicles.add_vehicle(
-        _Vehicle(LaneRef("first_road", "a"), 0.0), label="first_vehicle"
-    )
+    vehicles = VehiclePositions()
+    first_vehicle = vehicles.create_vehicle(LaneRef("first_road", "a"), 0.0)
+
     # ... second vehicle starts off at the other end on the second-road-b
-    vehicles.add_vehicle(
-        _Vehicle(LaneRef("second_road", "b"), 0.0), label="second_vehicle"
-    )
+    second_vehicle = vehicles.create_vehicle(LaneRef("second_road", "b"), 0.0)
 
     # WHEN i perform one step
-    stepper = Stepper(network)
-    vehicles = stepper.step(0.15, vehicles)
+    stepper = Stepper(network, vehicles)
+    stepper.step(0.15)
 
     # THEN the vehicles have moved
-    first_vehicle = vehicles.vehicle("first_vehicle")
-    second_vehicle = vehicles.vehicle("second_vehicle")
-    assert first_vehicle.lane_ref.junction == "first_road"
-    assert first_vehicle.lane_ref.lane == "a"
-    assert first_vehicle.position == pytest.approx(1.5)
-    assert second_vehicle.lane_ref.junction == "second_road"
-    assert second_vehicle.lane_ref.lane == "b"
-    assert second_vehicle.position == pytest.approx(3)
+    first_vehicle_data = vehicles[first_vehicle]
+    second_vehicle_data = vehicles[second_vehicle]
+    assert first_vehicle_data["lane_ref"].junction == "first_road"
+    assert first_vehicle_data["lane_ref"].lane == "a"
+    assert first_vehicle_data["position"] == pytest.approx(1.5)
+    assert second_vehicle_data["lane_ref"].junction == "second_road"
+    assert second_vehicle_data["lane_ref"].lane == "b"
+    assert second_vehicle_data["position"] == pytest.approx(3)
 
     # WHEN I keep stepping until the second vehicle is past the end of its road
-    vehicles = stepper.step(0.15, vehicles)  # position = 6
-    vehicles = stepper.step(0.15, vehicles)  # position = 9
-    vehicles = stepper.step(0.15, vehicles)  # position ... 12 > 10
+    stepper.step(0.15)  # position = 6
+    stepper.step(0.15)  # position = 9
+    stepper.step(0.15)  # position ... 12 > 10
 
     # THEN the first vehicle is still working its way along the slower road
-    first_vehicle = vehicles.vehicle("first_vehicle")
-    second_vehicle = vehicles.vehicle("second_vehicle")
-    assert first_vehicle.lane_ref.junction == "first_road"
-    assert first_vehicle.lane_ref.lane == "a"
-    assert first_vehicle.position == pytest.approx(6)
+    first_vehicle_data = vehicles[first_vehicle]
+    second_vehicle_data = vehicles[second_vehicle]
+    assert first_vehicle_data["lane_ref"].junction == "first_road"
+    assert first_vehicle_data["lane_ref"].lane == "a"
+    assert first_vehicle_data["position"] == pytest.approx(6)
 
     # ... AND the second vehicle has transitioned
-    assert second_vehicle.lane_ref.junction == "first_road"
-    assert second_vehicle.lane_ref.lane == "b"
-    assert second_vehicle.position == pytest.approx(1.0)
+    assert second_vehicle_data["lane_ref"].junction == "first_road"
+    assert second_vehicle_data["lane_ref"].lane == "b"
+    assert second_vehicle_data["position"] == pytest.approx(1.0)
 
     # WHEN we keep going until the first vehicle transitions
-    vehicles = stepper.step(0.15, vehicles)
+    stepper.step(0.15)
 
     # ... THEN the first vehicle has transitioned as expected
-    first_vehicle = vehicles.vehicle("first_vehicle")
-    second_vehicle = vehicles.vehicle("second_vehicle")
-    assert first_vehicle.lane_ref.junction == "second_road"
-    assert first_vehicle.lane_ref.lane == "a"
-    assert first_vehicle.position == pytest.approx(1.0)
+    first_vehicle_data = vehicles[first_vehicle]
+    second_vehicle_data = vehicles[second_vehicle]
+    assert first_vehicle_data["lane_ref"].junction == "second_road"
+    assert first_vehicle_data["lane_ref"].lane == "a"
+    assert first_vehicle_data["position"] == pytest.approx(1.0)
 
     # ... AND the second vehicle continues
-    assert second_vehicle.lane_ref.junction == "first_road"
-    assert second_vehicle.lane_ref.lane == "b"
-    assert second_vehicle.position == pytest.approx(2.5)
+    assert second_vehicle_data["lane_ref"].junction == "first_road"
+    assert second_vehicle_data["lane_ref"].lane == "b"
+    assert second_vehicle_data["position"] == pytest.approx(2.5)
 
     # WHEN we keep going until the vehicles finish
-    vehicles = stepper.step(0.15, vehicles)
-    vehicles = stepper.step(0.15, vehicles)
-    vehicles = stepper.step(0.1, vehicles)
-    vehicles = stepper.step(0.1, vehicles)
+    stepper.step(0.15)
+    stepper.step(0.15)
+    stepper.step(0.1)
+    stepper.step(0.1)
 
     with pytest.raises(KeyError):
-        first_vehicle = vehicles.vehicle("first_vehicle")
+        vehicles[first_vehicle]
 
     with pytest.raises(KeyError):
-        second_vehicle = vehicles.vehicle("second_vehicle")
+        vehicles[second_vehicle]
 
 
 def test_stops_if_vehicle_is_in_front():
     # GIVEN a single road with two vehicles
     network = Network(default_speed_limit=10)
     network.add_junction(Road((0, 0), 0, 100, 5))
-    vehicles = _VehiclesState()
-    vehicles.add_vehicle(_Vehicle(LaneRef("road1", "a"), 0), label="v1")
-    vehicles.add_vehicle(_Vehicle(LaneRef("road1", "a"), 4.5), label="v2")
+    vehicles = VehiclePositions()
+    v1 = vehicles.create_vehicle(LaneRef("road1", "a"), 0)
+    v2 = vehicles.create_vehicle(LaneRef("road1", "a"), 4.5)
 
     # WHEN I step
-    stepper = Stepper(network)
-    vehicles = stepper.step(0.1, vehicles)
+    stepper = Stepper(network, vehicles)
+    stepper.step(0.1)
 
     # THEN only the one in front moves
-    assert vehicles.vehicle("v1").position == pytest.approx(0.0)
-    assert vehicles.vehicle("v2").position == pytest.approx(5.5)
+    assert vehicles[v1]["position"] == pytest.approx(0.0)
+    assert vehicles[v2]["position"] == pytest.approx(5.5)
 
     # now the vehicle is far in front, on the next step both vehicles should move
-    vehicles = stepper.step(0.1, vehicles)
-    assert vehicles.vehicle("v1").position == pytest.approx(1.0)
-    assert vehicles.vehicle("v2").position == pytest.approx(6.5)
+    stepper.step(0.1)
+    assert vehicles[v1]["position"] == pytest.approx(1.0)
+    assert vehicles[v2]["position"] == pytest.approx(6.5)
 
 
 def test_stops_if_vehicles_are_on_top():
@@ -140,14 +137,14 @@ def test_stops_if_vehicles_are_on_top():
 
     network = Network(default_speed_limit=10)
     network.add_junction(Road((0, 0), 0, 100, 5))
-    vehicles = _VehiclesState()
-    vehicles.add_vehicle(_Vehicle(LaneRef("road1", "a"), 0), label="v1")
-    vehicles.add_vehicle(_Vehicle(LaneRef("road1", "a"), 0), label="v2")
+    vehicles = VehiclePositions()
+    v1 = vehicles.create_vehicle(LaneRef("road1", "a"), 0)
+    v2 = vehicles.create_vehicle(LaneRef("road1", "a"), 0)
 
     # WHEN I step
-    stepper = Stepper(network)
-    vehicles = stepper.step(0.1, vehicles)
+    stepper = Stepper(network, vehicles)
+    stepper.step(0.1)
 
     # THEN only the one in front moves
-    assert vehicles.vehicle("v1").position == pytest.approx(0.0)
-    assert vehicles.vehicle("v2").position == pytest.approx(1.0)
+    assert vehicles[v1]["position"] == pytest.approx(0.0)
+    assert vehicles[v2]["position"] == pytest.approx(1.0)
