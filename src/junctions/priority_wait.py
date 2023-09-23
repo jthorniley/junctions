be140d43413ee865ed1439c000835a29a893dd06
James Thorniley <james.thorniley@gmail.com>
2023-09-23 15:42:39 +0100

    shorter

--- src/junctions/priority_wait.py
+++ src/junctions/priority_wait.py
@@ -7,16 +7,16 @@ def priority_wait(network: Network, vehicle_positions: VehiclePositions) -> Wait
     """Calculate wait flags across network"""
     wait_flags = WaitFlags()
 
-    for current_lane_ref in network.all_lanes():
-        junction = network.junction(current_lane_ref.junction)
-        lane = junction.lanes[current_lane_ref.lane]
-        lane_clear_time = lane.length / network.speed_limit(current_lane_ref)
+    for lane_ref in network.all_lanes():
+        junction = network.junction(lane_ref.junction)
+        lane = junction.lanes[lane_ref.lane]
+        lane_clear_time = lane.length / network.speed_limit(lane_ref)
 
-        for priority_lane in junction.priority_over_lane(current_lane_ref.lane):
-            priority_lane_ref = LaneRef(current_lane_ref.junction, priority_lane)
+        for priority_lane in junction.priority_over_lane(lane_ref.lane):
+            priority_lane_ref = LaneRef(lane_ref.junction, priority_lane)
 
             if len(vehicle_positions.positions_by_lane[priority_lane_ref]):
-                wait_flags[current_lane_ref] = True
+                wait_flags[lane_ref] = True
                 break
 
             for feeder_lane_ref in network.feeder_lanes(priority_lane_ref):
@@ -35,10 +35,10 @@ def priority_wait(network: Network, vehicle_positions: VehiclePositions) -> Wait
                         feeder_lane.length - last_vehicle_position
                     ) / network.speed_limit(feeder_lane_ref)
                     if feeder_lane_vehicle_time_left < lane_clear_time:
-                        wait_flags[current_lane_ref] = True
+                        wait_flags[lane_ref] = True
                         break
 
-            if wait_flags[current_lane_ref]:
+            if wait_flags[lane_ref]:
                 # If any of the feeder lanes set the wait flag, we can break the outer
                 # loop - don't need to check any more priority lanes
                 break
